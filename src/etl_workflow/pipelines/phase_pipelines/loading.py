991a7faf2e5ee577935358b70925d143c7b09d9a
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-25 22:22:25 +0200

    Implement data loading and feature store integration
    Load environment variables, read transformed data, and interact with Hopsworks feature store to create a feature group and feature view.

--- src/etl_workflow/pipelines/phase_pipelines/loading.py
+++ src/etl_workflow/pipelines/phase_pipelines/loading.py
@@ -1,1 +1,44 @@
+import pandas as pd
+import os
+import hopsworks
+from dotenv import load_dotenv
+
+
+## load .env file:
+load_dotenv()
+
+## get the project name from .env file:
+project_name=os.getenv("PROJECT_NAME")
+api_key=os.getenv("API_KEY")
+
+## get the transformed data:
+df=pd.read_csv("transformed_df.csv")
+
+## login hopsworks
+project=hopsworks.login(
+    project=project_name,
+    api_key_value=api_key
+)
+
+## get the feature store:
+feature_store=project.get_feature_store()
+
+## get the feature view:
+feature_group=feature_store.get_or_create_group(
+    name="Prostate Feature Group",
+    description="View for the prostate ML project",
+    event_time="datetime",
+    primary_key="userID"
+)
+
+feature_group.insert(df,write_options={"wait_for_job":False})
+
+
+
+## Create the feature pipeline:
+feature_view=feature_store.get_or_create_feature_view(
+name="Prostate Feature View",
+version=1,
+labels=[],
+logging_enabled=True)
 
