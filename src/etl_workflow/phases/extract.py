915ee5d8472cedf8daa6958b1a552f66ed015cc6
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-25 22:09:27 +0200

    Implement data extraction and CSV export
    Extract data from the database and save to CSV.

--- src/etl_workflow/phases/extract.py
+++ src/etl_workflow/phases/extract.py
@@ -1,1 +1,26 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+## load the credentials of the database:
+user=os.getenv("USER")
+table_name=os.getenv("TABLE_NAME")
+schema_name=os.getenv("SCHEMA")
+password=os.getenv("PASSWORD")
+host = "127.0.0.1"
+port = "3306"
+user = "root"
+
+db_url = f"mysql+pymysql://{user}:{password}@{host}:{port}/{schema_name}"
+
+engine=create_engine(db_url)
+
+query="SELECT * FROM {table_name}"
+
+df=pd.read_sql_query(engine=engine,query=query)
+
+df.to_csv("data/extracted_df.csv")
