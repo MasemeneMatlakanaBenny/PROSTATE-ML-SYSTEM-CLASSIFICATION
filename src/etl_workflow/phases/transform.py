2b4f9e3172f6afdaa20ce4bd38321fbb53ece8be
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-25 22:09:48 +0200

    Transform data by adding userID and datetime
    Load extracted data, add unique userID and current datetime, then save the transformed DataFrame.

--- src/etl_workflow/phases/transform.py
+++ src/etl_workflow/phases/transform.py
@@ -1,1 +1,15 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime
 
+##load the extracted data:
+df=pd.read_csv("data/extracted_df.csv")
+
+## transform it by adding the unique userID and datetime:
+df['userID']=np.arange(1,len(df)+1)
+
+df['datetime']=datetime.now()
+
+
+## save the transformed dataframe to data folder:
+df.to_csv("data/transformed_df",index=False)
