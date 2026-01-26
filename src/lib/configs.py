4ff2d1125ed317c00ee5ac799a8b2650dbcc9523
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-26 05:14:56 +0200

    Update configs.py

--- src/lib/configs.py
+++ src/lib/configs.py
@@ -1,1 +1,17 @@
+import pandas as pd
+
+
+def X_train_y_train(train_df:pd.DataFrame):
+    """
+    Docstring for X_train_y_train
+    
+    :param train_df: Description
+    :type train_df: pd.DataFrame
+    """
+    label="Target"
+
+    X_train=train_df.drop(label,axis=1)
+    y_train=train_df[label]
+
+    return X_train,y_train
 
