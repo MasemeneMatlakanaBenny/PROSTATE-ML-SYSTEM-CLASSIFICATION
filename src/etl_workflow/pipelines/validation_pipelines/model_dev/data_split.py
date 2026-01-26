f25f4441d4124c0dd05033356adbb85c46144439
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-26 05:16:12 +0200

    Implement data splitting and saving for model training
    Load transformed data, split into train and test sets, and save to CSV.

--- src/etl_workflow/pipelines/validation_pipelines/model_dev/data_split.py
+++ src/etl_workflow/pipelines/validation_pipelines/model_dev/data_split.py
@@ -1,1 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+df=pd.read_csv("data/transformed_df.csv")
+
+## get the label:
+drop_columns=["datetime","userID"]
+
+df=df.drop(drop_columns,axis=1)
+
+##train and test sets
+train_df,test_df=train_test_split(df,test_size=0.2,random_state=42)
+
+
+## save both train and test sets to a csv format
+train_df.to_csv("data/train_df.csv",index=False)
+test_df.to_csv("data/test_df.csv",index=False)
 
