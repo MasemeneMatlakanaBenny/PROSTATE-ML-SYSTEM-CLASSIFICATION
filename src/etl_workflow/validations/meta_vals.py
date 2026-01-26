a6d8a9629b300bc42cc6a1a10620a5b90e7487d9
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-26 05:11:54 +0200

    Implement data validation workflow in meta_vals.py
    Load data from CSV, create a data batch, and run meta validations.

--- src/etl_workflow/validations/meta_vals.py
+++ src/etl_workflow/validations/meta_vals.py
@@ -1,1 +1,15 @@
+import pandas as pd
+from great_expectations.core.batch import Batch
+from typing import List
+from lib.validations import create_data_batch,meta_validations
 
+## load df:
+df=pd.read_csv("data_quality_checks/data_validation_results.csv")
+
+## create the batch:
+batch=create_data_batch(df=df)
+
+## create meta vals:
+meta_results=meta_validations(batch=batch)
+
+print(meta_results)
