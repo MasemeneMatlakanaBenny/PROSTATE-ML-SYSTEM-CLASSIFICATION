90a856eed4b1028d92176be381cf247836cf1565
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-28 21:05:35 +0200

    Update log_model_eval.py

--- src/model_evaluation/log_model_eval.py
+++ src/model_evaluation/log_model_eval.py
@@ -1,9 +1,10 @@
 import joblib
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from lib.configs import model_metrics,X_train_y_train
 
 ##load the model first:
-model=joblib.load("models/log_model.pkl")
+model:LogisticRegression=joblib.load("models/log_model.pkl")
 
 ## load X_test and y_test:
 test_df=pd.read_csv("data/test_df.csv")
