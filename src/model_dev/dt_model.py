877c48513ff4904497610757109c0bc81ab17a03
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-28 21:02:52 +0200

    Update dt_model.py

--- src/model_dev/dt_model.py
+++ src/model_dev/dt_model.py
@@ -1,1 +1,17 @@
+import pandas as pd
+import joblib
+from sklearn.tree import DecisionTreeClassifier
+from lib.configs import X_train_y_train
 
+train_df=pd.read_csv("data/train_df")
+
+## get the X_train and y_train sets:
+X_train,y_train=X_train_y_train(train_df=train_df)
+
+## train the model:
+model=DecisionTreeClassifier(criterion="entropy",min_samples_split=4)
+## fit the model:
+model.fit(X_train,y_train)
+
+## save the model:
+joblib.dump(model,"models/dt_model.pkl")
