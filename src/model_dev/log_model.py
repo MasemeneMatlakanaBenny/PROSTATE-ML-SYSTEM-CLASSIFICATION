5a2feacacf88c5be774cc1ea391b35f394412a4c
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-28 21:03:08 +0200

    Implement logistic regression model training and saving
    Added code to train and save a logistic regression model using training data.

--- src/model_dev/log_model.py
+++ src/model_dev/log_model.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import joblib
+from sklearn.linear_model import LogisticRegression
+from lib.configs import X_train_y_train
 
+train_df=pd.read_csv("data/train_df")
+
+## get the X_train and y_train sets:
+X_train,y_train=X_train_y_train(train_df=train_df)
+
+## train the model:
+model=LogisticRegression(solver="liblinear")
+
+## fit the model:
+model.fit(X_train,y_train)
+
+## save the model:
+joblib.dump(model,"models/log_model.pkl")
