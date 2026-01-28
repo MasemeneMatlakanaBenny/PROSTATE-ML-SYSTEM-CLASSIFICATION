ed82028b42c7d38a8dd1cb4b990c9f13002bffee
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-28 21:06:08 +0200

    Update dt_model_eval.py

--- src/model_evaluation/dt_model_eval.py
+++ src/model_evaluation/dt_model_eval.py
@@ -1,9 +1,10 @@
 import joblib
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 from lib.configs import model_metrics,X_train_y_train
 
 ##load the model first:
-model=joblib.load("models/dt_model.pkl")
+model:DecisionTreeClassifier=joblib.load("models/dt_model.pkl")
 
 ## load X_test and y_test:
 test_df=pd.read_csv("data/test_df.csv")
