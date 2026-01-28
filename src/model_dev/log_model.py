68d49fe1d2c9fd8cd3c44f3d11c2a2605ac7ef49
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-28 21:07:49 +0200

    Update log_model.py

--- src/model_dev/log_model.py
+++ src/model_dev/log_model.py
@@ -3,7 +3,7 @@ import joblib
 from sklearn.linear_model import LogisticRegression
 from lib.configs import X_train_y_train
 
-train_df=pd.read_csv("data/train_df")
+train_df:pd.DataFrame=pd.read_csv("data/train_df")
 
 ## get the X_train and y_train sets:
 X_train,y_train=X_train_y_train(train_df=train_df)
