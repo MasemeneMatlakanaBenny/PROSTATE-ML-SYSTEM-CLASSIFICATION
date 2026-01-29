cbc442ba7f30cee231d2e8f91cc4537ecae974e8
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-29 22:17:05 +0200

    Update train_pipeline.py

--- src/pipelines/train_pipeline.py
+++ src/pipelines/train_pipeline.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from prefect import task,flow
+from typing import Union
+from lib.configs import X_train_y_train
 
+@task
+def get_train_df(path:str="data/train_df")->pd.DataFrame:
+    """
+    """
+    
+    train_df=pd.read_csv("data/train_df")
+    return train_df
