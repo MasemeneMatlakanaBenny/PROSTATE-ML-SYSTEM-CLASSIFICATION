ef6989ca84a4e70b807cfac02e78cf2c3f608b20
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-29 22:26:33 +0200

    imported libs

--- src/pipelines/model_evaluation_pipeline.py
+++ src/pipelines/model_evaluation_pipeline.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from prefect import task,flow
+from typing import Union
+from lib.configs import X_train_y_train
+
 
