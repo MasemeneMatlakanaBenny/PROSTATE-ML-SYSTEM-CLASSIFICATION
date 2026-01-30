c36a504b9175879e956f310ba9bd9ebb213b0a9b
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-30 09:03:41 +0200

    get trained models

--- src/pipelines/model_validation_pipeline.py
+++ src/pipelines/model_validation_pipeline.py
@@ -5,5 +5,17 @@ from sklearn.tree import DecisionTreeClassifier
 from prefect import task,flow
 from typing import Union,Dict
 
+@task
+def load_model(path:str)->Union[LogisticRegression,DecisionTreeClassifier]:
+    """
+    Docstring for load_model
+    
+    :param path: Description
+    :type path: str
+    :return: Description
+    :rtype: LogisticRegression | DecisionTreeClassifier
+    """
 
+    model=joblib.load(path)
 
+    return model
