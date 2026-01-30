32da3d9dc604fb2a578b4c95bc77209e15290cde
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-30 09:03:17 +0200

    import libs

--- src/pipelines/model_validation_pipeline.py
+++ src/pipelines/model_validation_pipeline.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from prefect import task,flow
+from typing import Union,Dict
+
+
 
