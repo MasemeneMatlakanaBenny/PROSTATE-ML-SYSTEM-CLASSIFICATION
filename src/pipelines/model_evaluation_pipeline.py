3b379d68e2ea42f019b925078be46e336defd65d
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-30 08:52:57 +0200

    added docstring for load function

--- src/pipelines/model_evaluation_pipeline.py
+++ src/pipelines/model_evaluation_pipeline.py
@@ -9,7 +9,16 @@ from lib.configs import X_train_y_train
 
 
 ##load the model first:
+@task
 def load_model(path:str)->Union[LogisticRegression,DecisionTreeClassifier]:
+     """
+    Docstring for load_model
+    
+    :param path: Description
+    :type path: str
+    :return: Description
+    :rtype: LogisticRegression | DecisionTreeClassifier
+    """
 
     model=joblib.load(path)
     return model
